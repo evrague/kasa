7f7da38550ad0be9b91e9aaafe03e9f00b29d2c6
évrague <jpfoucaux@gmail.com>
2025-02-27 20:31:37 +0100

    Version finale corrigée

--- kasa-react/src/Components/Apropos.jsx
+++ kasa-react/src/Components/Apropos.jsx
@@ -11,10 +11,10 @@ export default function Apropos(){
         const [visible, setVisible] =  useState([false, false, false, false]);
 
         const tirroirs = [
-                {title: "Fiablité", content:"Je suis invisible Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, adipisci! Hic ullam repellendus ratione dolores itaque consequatur sapiente amet illo accusantium, mollitia aspernatur. Suscipit, voluptatum reiciendis quos non dolores necessitatibus."},
-                {title: "Respect", content:"Je suis invisible Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, adipisci! Hic ullam repellendus ratione dolores itaque consequatur sapiente amet illo accusantium, mollitia aspernatur. Suscipit, voluptatum reiciendis quos non dolores necessitatibus."},
-                {title: "Service", content:"Je suis invisible Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, adipisci! Hic ullam repellendus ratione dolores itaque consequatur sapiente amet illo accusantium, mollitia aspernatur. Suscipit, voluptatum reiciendis quos non dolores necessitatibus."},
-                {title: "Securité", content:"Je suis invisible Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, adipisci! Hic ullam repellendus ratione dolores itaque consequatur sapiente amet illo accusantium, mollitia aspernatur. Suscipit, voluptatum reiciendis quos non dolores necessitatibus."}
+                {title: "Fiablité", content:"Les annonces postées sur kasa garantissent une fiabilité totale. Les photos sont conformes aux logements et toutes les informations sont régulièrement vérifées par nos équipes."},
+                {title: "Respect", content:"La bienveillance fait partie des valeurs fondatrices de kasa. Tout comportement discriminatoire ou de perturbation du voisinage entrainera une exclusion de notre plateforme."},
+                {title: "Service", content:"La qualité du service est au coeur de notre engagement chez kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires soit empreinte de respect et de bienveillance."},
+                {title: "Securité", content:"La sécurité est la priorité de kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."}
         ]
 
         const toggleVisible = (index)=> {
